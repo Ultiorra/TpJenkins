5931eaf415f1baff733ac27c7e4a4c68eb759eb6
mj396287 <jarreaumik@hotmail.com>
2023-03-28 16:52:36 +0200

    tests parametrées

--- src/test/java/book/TestBook.java
+++ src/test/java/book/TestBook.java
@@ -2,20 +2,58 @@ package book;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Date;
+import java.util.stream.Stream;
 
 import static org.testng.Assert.assertEquals;
 
 public class TestBook {
 
-    @MethodSource("book.Book")
-    @Test
-    public void testBook() {
-        Book book = new Book("The Lord of the Rings", "J.R.R. Tolkien", new Date(1954, 1, 1));
-        assertEquals("The Lord of the Rings", book.getTitle());
-        assertEquals("J.R.R. Tolkien", book.getAuthor());
-        assertEquals(new Date(1954, 1, 1), book.getPublished());
+    public static Stream<Arguments> getTitles() {
+        return Stream.of(
+                Arguments.of("title1"),
+                Arguments.of("title2"),
+                Arguments.of("title3")
+        );
+    }
+
+    public static Stream<Arguments> getAuthors() {
+        return Stream.of(
+                Arguments.of("author1"),
+                Arguments.of("author2"),
+                Arguments.of("author3")
+        );
+    }
+
+    public static Stream<Arguments> getPublished() {
+        return Stream.of(
+                Arguments.of(new Date()),
+                Arguments.of(new Date()),
+                Arguments.of(new Date())
+        );
+    }
+    @MethodSource("getTitles")
+    @org.junit.jupiter.params.ParameterizedTest
+    public void testGetTitle(String title) {
+        Book b = new Book(title, "author", new Date());
+        assertEquals(title, b.getTitle());
+    }
+
+    @MethodSource("getAuthors")
+    @org.junit.jupiter.params.ParameterizedTest
+    public void testGetAuthor(String author) {
+        Book b = new Book("title", author, new Date());
+        assertEquals(author, b.getAuthor());
+    }
+
+    @MethodSource("getPublished")
+    @org.junit.jupiter.params.ParameterizedTest
+    public void testGetPublished(Date published) {
+        Book b = new Book("title", "author", published);
+        assertEquals(published, b.getPublished());
     }
 }
